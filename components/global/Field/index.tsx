5b16f86801f836b46a334eeb585c41a4dd8fff63
Andrew Osei-Wusu <andrewosei94@gmail.com>
2026-08-12 15:53:20 +0000

    purge project

--- components/global/Field/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./error";
-export * from "./group";
-export * from "./input";
-export * from "./place";
-export * from "./phone";
-export { default as Select } from "./Select/Select";
-export * from "./TextEditor";
